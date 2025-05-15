30648c55f8bec63961966127d22424e6b05c737f
q1hu1 <2430139130@qq.com>
2025-05-15 18:30:08 +0800

    成绩录入 api接口管理 通知公告发布

--- ruoyi-xscjxxgl/src/main/java/com/ruoyi/xscj/cjlr/service/ICjlrService.java
+++ ruoyi-xscjxxgl/src/main/java/com/ruoyi/xscj/cjlr/service/ICjlrService.java
@@ -1,4 +1,74 @@
 // Todo WJS
 // Todo LJY
-// Todo SXT
 // Todo SJB
+package com.ruoyi.xscj.cjlr.service;
+
+import java.util.List;
+
+import com.ruoyi.xscj.cjlr.domain.BjCj;
+import com.ruoyi.xscj.cjlr.domain.CjTj;
+import com.ruoyi.xscj.cjlr.domain.Cjlr;
+import com.ruoyi.xscj.cjlr.domain.Gpa;
+
+/**
+ * 成绩录入Service接口
+ *
+ */
+public interface ICjlrService{
+    /**
+     * 查询成绩录入
+     *
+     * @param cjlrId 成绩录入主键
+     * @return 成绩录入
+     */
+    public Cjlr selectCjlrByCjlrId(String cjlrId);
+
+    /**
+     * 查询成绩录入列表
+     *
+     * @param cjlr 成绩录入
+     * @return 成绩录入集合
+     */
+    public List<Cjlr> selectCjlrList(Cjlr cjlr);
+
+    /**
+     * 新增成绩录入
+     *
+     * @param cjlr 成绩录入
+     * @return 结果
+     */
+    public int insertCjlr(Cjlr cjlr);
+
+    /**
+     * 批量新增成绩录入
+     *
+     * @param cjlrs 成绩录入List
+     * @return 结果
+     */
+    public int batchInsertCjlr(List<Cjlr> cjlrs);
+
+    /**
+     * 修改成绩录入
+     *
+     * @param cjlr 成绩录入
+     * @return 结果
+     */
+    public int updateCjlr(Cjlr cjlr);
+
+    /**
+     * 批量删除成绩录入
+     *
+     * @param cjlrIds 需要删除的成绩录入主键集合
+     * @return 结果
+     */
+    public int deleteCjlrByCjlrIds(String[] cjlrIds);
+
+    /**
+     * 删除成绩录入信息
+     *
+     * @param cjlrId 成绩录入主键
+     * @return 结果
+     */
+    public int deleteCjlrByCjlrId(String cjlrId);
+
+}
